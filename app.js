9615232afdf7cffaf45e638446d519c10e818387
Daniel2711 <detaddeo.d@gmail.com>
2020-12-11 18:35:30 +0100

    Pokemon immagine inserita

--- app.js
+++ app.js
@@ -12,18 +12,42 @@ const fetchData = async ()=> {
     
     try{
 
-        var number = getRandomInt(1, 150);
-        console.log(number);
+        var id = getRandomInt(1, 150);
+        console.log(id);
 
-        const res = await fetch('https://pokeapi.co/api/v2/pokemon/'+number);
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon/'+id);
         const data = await res.json();
-        console.log(data.name);
 
-    } catch(error){
-        console.log(error)
+        console.log(data.id + ' : ' + data.name);
+
+        pintarCard(data);
+
+    } catch(e){
+        console.log(e);
     }
 
 }
 
 // starting page
 $(document).ready(fetchData());
+
+
+// pintar carta
+const pintarCard = (pokemon) => {
+    // test ingresso
+    console.log(pokemon);
+    // dove lo metto
+    const flex = document.querySelector('.flex');
+    // cattura del template
+    const template = document.querySelector('#template-card').content;
+    // clona template
+    const clone = template.cloneNode(true);
+    // fare un fragment
+    const fragment = document.createDocumentFragment();
+
+    clone.querySelector('.card-body img').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
+    
+    fragment.appendChild(clone);
+
+    flex.appendChild(fragment);
+}
