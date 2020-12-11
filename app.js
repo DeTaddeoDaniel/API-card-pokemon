16c7f23c4771c7042a9ebdb40b4bc6eca23c933f
Daniel2711 <detaddeo.d@gmail.com>
2020-12-11 17:49:33 +0100

    Chiama API pokemon in console nome pokemon random

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const fetchData = async ()=> {
 
         const res = await fetch('https://pokeapi.co/api/v2/pokemon/'+number);
         const data = await res.json();
-        console.log(data);
+        console.log(data.name);
 
     } catch(error){
         console.log(error)
